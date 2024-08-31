a5f2bd4bf6d75c67c31081f94e83e2de254d732b
Sunil Kanugula <kanugulasunilkumar0511@gmail.com>
2024-08-31 10:56:20 +0530

    second commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (<div>
     <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div >
-            <img src={assets.logo} alt="logo" className='mb-5 w-32'/>
+            <h1  className='mb-5 w-32 text-3xl'>WOLFY</h1>
             <p className='w-full '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, doloribus id non temporibus iure magnam architecto error iste eligendi veniam fuga exercitationem officiis tempore amet placeat eveniet illo cupiditate ut!</p>
         </div>
 
