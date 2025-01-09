d989c0b4c09d50d67969059e1b2d78139ff1adb1
Sunil Kanugula <kanugulasunilkumar0511@gmail.com>
2025-01-09 11:38:51 +0530

    sensitive case of productRoute.js debugged

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
-import productRouter from "./routes/ProductRoute.js"
+import productRouter from "./routes/productRoute.js"
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
  
